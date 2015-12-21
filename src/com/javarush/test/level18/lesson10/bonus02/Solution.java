745deccbd3442a8730876ec0b8915dd2649c97e4
Vladimir Belyashov <k51201@vampa.ru>
2015-12-21 20:23:14 +0500

    Написан код level18.lesson10.bonus02 Не проверен

--- src/com/javarush/test/level18/lesson10/bonus02/Solution.java
+++ src/com/javarush/test/level18/lesson10/bonus02/Solution.java
@@ -22,7 +22,56 @@ id productName price quantity
 19847983Куртка для сноубордистов, разм10173.991234
 */
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
+        File tableFile = new File(cin.readLine());
+        cin.close();
+
+        BufferedReader fin = new BufferedReader(new FileReader(tableFile));
+        List<String> lines = new ArrayList<>();
+        int maxId = 0;
+        while (fin.ready()) {
+            String currLine = fin.readLine();
+            if (!currLine.isEmpty()) {
+                lines.add(currLine);
+                int currId = Integer.parseInt(currLine.substring(0, 7));
+                if (maxId < currId)
+                    maxId = currId;
+            }
+        }
+        fin.close();
+
+        if (args[0].equals("-c")) {
+            StringBuilder newLine = new StringBuilder(50);
+            String id = trimLineToSize(Integer.toString(maxId + 1), 8);
+            String productName = trimLineToSize(args[1], 30);
+            String price = trimLineToSize(args[2], 8);
+            String quantity = trimLineToSize(args[3], 4);
+            newLine.append(id).append(productName).append(price).append(quantity);
+            lines.add(newLine.toString());
+        }
+
+        PrintWriter fout = new PrintWriter(tableFile);
+        for (String line : lines)
+            fout.println(line);
+    }
+
+    private static String trimLineToSize(String line, int size) {
+        String resultLine;
+        if (size < line.length())
+            resultLine = line.substring(0, size);
+        else {
+            int missingSpaces = size - line.length();
+            StringBuilder lineBuffer = new StringBuilder(line);
+            for (int i = 0; i < missingSpaces; i++)
+                lineBuffer.append(' ');
+            resultLine = lineBuffer.toString();
+        }
+        return resultLine;
     }
 }
