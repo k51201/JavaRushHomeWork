ca028981419f845fa7a0b9f51bdfb30f3e05db6f
Vladimir Belyashov <k51201@vampa.ru>
2016-01-08 04:12:21 +0500

    Решено level20.lesson10.home06

---
+++ src/com/javarush/test/level20/lesson10/home06/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level20.lesson10.home06;
+
+import java.io.*;
+
+/* Запрет сериализации
+Запретите сериализацию класса SubSolution используя NotSerializableException.
+Сигнатуры классов менять нельзя
+*/
+public class Solution implements Serializable {
+    public static class SubSolution extends Solution {
+        private void writeObject(ObjectOutputStream out) throws NotSerializableException {
+            throw new NotSerializableException(this.getClass().toString());
+        }
+
+        private void readObject(ObjectInputStream in) throws NotSerializableException {
+            throw new NotSerializableException(this.getClass().toString());
+        }
+    }
+}
